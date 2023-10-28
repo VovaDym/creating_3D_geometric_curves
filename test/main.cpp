adc4a1b2daa6aaa685ef7d9eda6f4ea490e45d84
Vladimir Dymov <77827028+VovaDym@users.noreply.github.com>
2023-10-28 12:35:56 +0300

    Добавьте файлы проекта.

---
+++ test/main.cpp
@@ -0,0 +1,149 @@
+﻿// test.cpp: определяет точку входа для приложения.
+//
+#include <iostream>
+#include "shape_creating.h"
+#include <tbb/parallel_for.h>
+#include <random>
+#include <cmath>
+#include <memory>
+#include <typeinfo>
+#include <algorithm>
+
+using PtrShape = std::shared_ptr<Shape>; 
+using PtrCircle = std::shared_ptr<Circle>;  
+constexpr double eps = 1e-6;
+constexpr size_t n = 10;   
+const double PI = std::acos(-1);    
+char typeC{}, typeE{}, typeH{};
+
+static std::random_device rd;
+
+class ShapeGenerator  
+{
+    enum shapeType 
+    {
+        circleType, ellipseType, helixType
+    };
+
+    std::uniform_real_distribution<double> udd{ -1, 1 };    
+    std::uniform_int_distribution<> usd{ circleType, helixType };   
+
+public:
+
+    PtrShape circle()
+    {   
+        double radius{};
+        while (radius < eps) 
+        {
+            radius = std::abs(udd(rd)); 
+        }
+
+        return std::make_shared<Circle>(Point{ udd(rd), udd(rd) }, radius);
+    }
+
+    PtrShape ellipse()
+    {   
+        double radius_x{};
+        while (radius_x < eps) 
+        {
+            radius_x = std::abs(udd(rd));   
+        }
+
+        double radius_y{};
+        while (radius_y < eps)
+        {
+            radius_y = std::abs(udd(rd));   
+        }
+
+        return std::make_shared<Ellipse>(Point{ udd(rd), udd(rd) }, radius_x, radius_y);
+    }
+
+    PtrShape helix()
+    {   
+        double radius{};
+        while (radius < eps)
+        {
+            radius = std::abs(udd(rd)); 
+        }
+
+        double step{};
+        while (std::abs(step) < eps) 
+        {
+            step = udd(rd);
+        }
+
+        return std::make_shared<Helix>(Point{ udd(rd), udd(rd), udd(rd) }, radius, step);
+    }
+
+    PtrShape random_shape()
+    {  
+
+        switch (usd(rd))
+        {
+        case circleType:
+            typeC = 'y';
+            return circle();
+
+        case ellipseType:
+            typeE = 'y';
+            return ellipse();
+        case helixType:
+            typeH = 'y';
+            return helix();
+        default:
+            return circle(); 
+        }
+    }
+};
+
+int main()
+{
+    std::vector<PtrShape> shapeVec;  
+
+    ShapeGenerator shapeGen; 
+    
+    do
+    {
+        for (size_t i{}; i < n; ++i)
+        {
+            shapeVec.emplace_back(shapeGen.random_shape());   
+        }
+    } while (typeC != 'y' && typeE != 'y' && typeH != 'y');
+    
+
+    std::vector<PtrCircle> circleVec;    
+    double radiusSum{};    
+
+    for (auto& ptrType : shapeVec)
+    {
+        const std::type_info& tInfo{ typeid(*ptrType) };    
+
+        std::cout << tInfo.name() << ' ' << ptrType->curvePoint(PI / 4) << ' '  
+                  << ptrType->curveVector(PI / 4) << std::endl;   
+
+        if (tInfo.hash_code() == typeid(Circle).hash_code()) 
+        {
+            circleVec.emplace_back(std::dynamic_pointer_cast<Circle>(ptrType));             
+        }
+        std::cout << std::endl;
+    }
+
+    tbb::parallel_for(tbb::blocked_range<size_t>(0, circleVec.size()),
+        [&](const tbb::blocked_range<size_t>& r) {
+            for (size_t i = r.begin(); i < r.end(); ++i) {
+                radiusSum += circleVec[i]->radius();
+            }
+        });
+    std::sort(circleVec.begin(), circleVec.end(),
+        [](const auto& l, const auto& r)  
+        {
+            return l->radius() < r->radius();
+        });
+
+    for (auto& ptrCircle : circleVec)
+    {
+        std::cout << ptrCircle->radius() << std::endl;
+    }
+
+    std::cout << "sum = " << radiusSum << std::endl;   
+}
---
+++ test/shape_creating.h
@@ -0,0 +1,140 @@
+﻿// test.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+
+#pragma once
+
+#include <iostream>
+
+struct Point 
+{
+    double x{}, y{}, z{};
+};
+
+std::ostream& operator<<(std::ostream& os, const Point& p)
+{   
+    return os << "point{ " << p.x << ", " << p.y << ", " << p.z << " }";
+}
+
+struct Vector 
+{
+    double x{}, y{}, z{};
+};
+
+std::ostream& operator<<(std::ostream& os, const Vector& v)
+{   
+    return os << "vector{ " << v.x << ", " << v.y << ", " << v.z << " }";
+}
+
+class Shape   
+{
+protected:
+    Shape() = default;
+
+public:
+    virtual ~Shape() {}
+
+    virtual Point curvePoint(double t) const = 0;    
+    virtual Vector curveVector(double t) const = 0;  
+};
+
+class Circle : public Shape 
+{
+    double radiusCircle{};
+    const Point supportPointCir;
+
+public:
+    Circle() = default;
+    Circle(const Point& point, double radius)
+        : supportPointCir(point), radiusCircle{ radius } {}
+
+    virtual ~Circle() {}
+
+    double radius() const 
+    {
+        return radiusCircle;
+    }
+
+    virtual Point curvePoint(double t) const override
+    {
+        return Point
+        {
+            radiusCircle * std::cos(t) + supportPointCir.x,
+            radiusCircle * std::sin(t) + supportPointCir.y,
+            supportPointCir.z
+        };
+    }
+    virtual Vector curveVector(double t) const override
+    {
+        return Vector
+        {
+            -1 * radiusCircle * std::sin(t),
+            radiusCircle * std::cos(t)
+        };
+    }
+};
+
+class Ellipse : public Shape   
+{
+    double radiusX{};
+    double radiusY{};
+    const Point supportPointEll;
+
+public:
+    Ellipse() = default;
+    Ellipse(const Point& point, double radius_x, double radius_y)
+        : supportPointEll{ point }, radiusX{ radius_x }, radiusY{ radius_y } {}
+
+    virtual ~Ellipse() {}
+
+    virtual Point curvePoint(double t) const override
+    {
+        return Point
+        {
+            radiusX * std::cos(t) + supportPointEll.x,
+            radiusY * std::sin(t) + supportPointEll.y,
+            supportPointEll.z
+        };
+    }
+    virtual Vector curveVector(double t) const override
+    {
+        return Vector
+        {
+            -1 * radiusX * std::sin(t),
+            radiusY * std::cos(t)
+        };
+    }
+};
+
+class Helix : public Shape  
+{
+    double radiusHel{};
+    double stepZ{};
+    const Point supportPointHel;
+
+public:
+    Helix() = default;
+    Helix(const Point& point, double radius, double step)
+        : supportPointHel{ point }, radiusHel{ radius }, stepZ{ step } {}
+
+    virtual ~Helix() {}
+
+    virtual Point curvePoint(double t) const override
+    {
+        return Point
+        {
+            radiusHel * t * std::cos(t) + supportPointHel.x,
+            radiusHel * t * std::sin(t) + supportPointHel.y,
+            stepZ * t + supportPointHel.z
+        };
+    }
+    virtual Vector curveVector(double t) const override
+    {
+        return Vector
+        {
+            radiusHel * (std::cos(t) - t * std::sin(t)),
+            radiusHel * (std::sin(t) + t * std::cos(t)),
+            stepZ
+        };
+    }
+};
+
